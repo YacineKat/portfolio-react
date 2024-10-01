5febfcb4e1e54f7c8355d03bc423dcbed5510cf9
YacineKat <yassine.katrouci@etu.univ-mosta.dz>
2024-10-01 19:59:10 +0100

    Portfolio reaxt

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import '../header/Header.css'
 import pic from '../../assets/profil-pic.png'
 import plus from '../../assets/plus.svg'
 import copy from '../../assets/copy.svg'
 const Header = () => {
+  const email = "yacinekatrouci@gmail.com";
+  const [copied, setCopied] = useState(false); 
+
+  const handleCopyEmail = () => {
+    navigator.clipboard.writeText(email)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+      })
+      .catch(err => {
+        console.error('Failed to copy email: ', err);
+      });
+  };
   return (
     <div>
-      <header className="Header-Cont">
+      <header className="Header-Cont" id='about'>
         <div className="Info">
             <div className="Job">
                 <h2>Web Developer</h2>
@@ -30,22 +44,25 @@ const Header = () => {
                 </div>
             </div>
         </div>
-        <div className="buttons">
-          <button className="hire-me">
-            Hire me
-            <div className="icon-button">
+        <div className="Buttons">
+          <button className="Hire-Me">
+            <p>
+              Hire me
+            </p>
+            <div className="Icon-Button Plus">
               <img src={plus} alt="plus icon" />
             </div>
           </button>
-          <button className="copy-email">
-            Copy Email
-            <div className="icon-button">
+          <button className="Copy-Email" onClick={handleCopyEmail}>
+            <p>
+              Copy Email
+            </p>
+            <div className="Icon-Button Email">
               <img src={copy} alt="copy icon" />
             </div>
           </button>
+          {copied && <div className="Copy-Message">Email copied!</div>} 
         </div>
-
-
       </header>
     </div>
   )
